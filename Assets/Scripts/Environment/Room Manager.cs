37cdd9885a011862b911ffdc38508ffcf32fdc50
jjwhite44 <jeremy.white@richmond.edu>
2024-07-09 15:43:26 -0400

    initial commit

---
+++ Assets/Scripts/Environment/Room Manager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class RoomManager : MonoBehaviour
+{
+    public UnityEvent winner;
+
+    public void Winner()
+    {
+        if (true == true) //this is a placeholder, fill in whatever win condition here
+        {
+            Debug.Log("You win!");
+            winner.Invoke(); //assigned in inspector window, this is where you could make a door "open" (disappear)
+        }
+    }
+}
